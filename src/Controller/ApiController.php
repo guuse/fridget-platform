2b3a70f6fc473ae1cd05b866585344ea0ada4382
Guus Ekkelenkamp <guus@connectholland.nl>
2020-01-27 11:30:39 +0100

    Add api doc

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -10,6 +10,7 @@ use App\Entity\Products;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Swagger\Annotations as SWG;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -38,11 +39,17 @@ final class ApiController extends AbstractController
 
     /**
      * @Rest\Get("/users", name="findAllUsers")
+     * @SWG\Response(
+     *     response=200,
+     *     description="Gets all users"
+     * )
+     * @SWG\Tag(name="Users")
      */
     public function findAllUsersAction(): JsonResponse
     {
         $users = $this->em->getRepository(User::class)->findBy([], ['id' => 'DESC']);
         $data = $this->serializer->serialize($users, JsonEncoder::FORMAT, [
+                AbstractNormalizer::IGNORED_ATTRIBUTES => ['user', 'plainPassword', 'salt', 'password', 'box', 'roles'],
                 'circular_reference_handler' => function ($object) {
                     return $object->getId();
                 }]
@@ -53,6 +60,18 @@ final class ApiController extends AbstractController
 
     /**
      * @Rest\Get("/ean/{ean}", name="findEanProductInfo")
+     * @SWG\Response(
+     *     response=200,
+     *     description="Get detailed informmation about a product"
+     * )
+     * @SWG\Parameter(
+     *     name="ean",
+     *     in="path",
+     *     type="string",
+     *     required=true,
+     *     description="The ean of the requested product, eg: 8719987324772"
+     * )
+     * @SWG\Tag(name="Ean")
      */
     public function findEanProduct($ean): JsonResponse
     {
@@ -68,6 +87,18 @@ final class ApiController extends AbstractController
 
     /**
      * @Rest\Get("/{uuid}/boxes", name="findAllBoxes")
+     * @SWG\Response(
+     *     response=200,
+     *     description="Get all boxes from a user"
+     * )
+     * @SWG\Parameter(
+     *     name="uuid",
+     *     in="path",
+     *     type="string",
+     *     required=true,
+     *     description="The uuid of the user, eg: 10d2327b-20ad-11ea-8a5a-0242c0a86003"
+     * )
+     * @SWG\Tag(name="Boxes")
      */
     public function findAllBoxesAction($uuid): JsonResponse
     {
@@ -86,6 +117,18 @@ final class ApiController extends AbstractController
 
     /**
      * @Rest\Get("/{boxId}/products", name="findAllProducts")
+     * @SWG\Response(
+     *     response=200,
+     *     description="Gets all products in a box"
+     * )
+     * @SWG\Parameter(
+     *     name="boxId",
+     *     in="path",
+     *     type="string",
+     *     required=true,
+     *     description="The id of the requested box, eg: 434"
+     * )
+     * @SWG\Tag(name="Products")
      */
     public function findAllProductsAction($boxId): JsonResponse
     {
@@ -106,11 +149,78 @@ final class ApiController extends AbstractController
      * @throws \Exception
      *
      * @Rest\Post("/add/product", name="createProduct")
+     * @SWG\Response(
+     *     response=201,
+     *     description="Create new product"
+     * )
+     * @SWG\Parameter(
+     *     name="body",
+     *     in="body",
+     *     required=true,
+     *     @SWG\Schema(
+     *              @SWG\Property(
+     *                  property="box",
+     *                  type="integer",
+     *                  description="The ID of the box to add your product in",
+     *                  example="434"
+     *              ),
+     *              @SWG\Property(
+     *              property="products",
+     *              type="array",
+     *              description="The product that should be added",
+     *              @SWG\Items(
+     *                  @SWG\Property(
+     *                      property="name",
+     *                      type="string",
+     *                      description="The name of the product",
+     *                      example="Worst"
+     *                  ),
+     *                  @SWG\Property(
+     *                      property="desc",
+     *                      type="string",
+     *                      description="The description of the product",
+     *                      example="Grote & sappige bloedworst"
+     *                  ),
+     *                  @SWG\Property(
+     *                      property="amount",
+     *                      type="integer",
+     *                      description="The amount added to the box",
+     *                      example=2
+     *                  ),
+     *                  @SWG\Property(
+     *                      property="expires",
+     *                      type="integer",
+     *                      description="The days until the product expires",
+     *                      example=12
+     *                  ),
+     *                  @SWG\Property(
+     *                      property="unit",
+     *                      type="string",
+     *                      description="The unit with amount of the product",
+     *                      example="200g"
+     *                  ),
+     *                  @SWG\Property(
+     *                      property="category",
+     *                      type="string",
+     *                      description="The category of the product",
+     *                      example="meat"
+     *                  )
+     *              )
+     *          )
+     *
+     *     )
+     *
+     * )
+     * @SWG\Tag(name="Products")
      */
     public function createProductAction(Request $request): JsonResponse
     {
         $box = $this->em->getRepository(Box::class)->findOneBy(array('id' => $request->request->get('box')));
         $products = $request->request->get('products');
+
+        if (empty($products)) {
+            throw new BadRequestHttpException('Can\'t find a product to add');
+        }
 
         foreach ($products as $product) {
             $name = $product['name'];
@@ -120,8 +230,8 @@ final class ApiController extends AbstractController
             $unit = $product['unit'];
             $category = $product['category'];
 
-            if (empty($name) || empty($amount) || empty($expires)) {
-                throw new BadRequestHttpException('Can\'t find name, amount or expires in the body');
+            if (empty($name) || empty($amount) || empty($expires) || empty($category)) {
+                throw new BadRequestHttpException('Can\'t find name, amount, expires or category in the product');
             }
 
             $productEntity = new Products();
@@ -151,6 +261,18 @@ final class ApiController extends AbstractController
      * @throws \Exception
      *
      * @Rest\Delete("/remove/product/{id}", name="deleteProduct")
+     * @SWG\Response(
+     *     response=204,
+     *     description="Delete a product from the box"
+     * )
+     * @SWG\Parameter(
+     *     name="id",
+     *     in="path",
+     *     type="integer",
+     *     required=true,
+     *     description="The id of the product that will be deleted"
+     * )
+     * @SWG\Tag(name="Products")
      */
     public function deleteProductAction(Request $request, $id): JsonResponse
     {
@@ -170,6 +292,31 @@ final class ApiController extends AbstractController
      * @throws \Exception
      *
      * @Rest\Put("/update/{id}", name="updateProduct")
+     * @SWG\Response(
+     *     response=200,
+     *     description="Create new product"
+     * )
+     * @SWG\Parameter(
+     *     name="id",
+     *     in="path",
+     *     type="integer",
+     *     required=true,
+     *     description="The id of the product that changes"
+     * )
+     * @SWG\Parameter(
+     *     name="body",
+     *     in="body",
+     *     required=true,
+     *     @SWG\Schema(
+     *         @SWG\Property(
+     *             property="amount",
+     *             type="integer",
+     *             description="The new amount of the product in the box",
+     *             example=3
+     *         )
+     *     )
+     * )
+     * @SWG\Tag(name="Products")
      */
     public function updateProductAction(Request $request, $id): JsonResponse
     {
